92e918121a4bffb507585719d50fadeecf1030f9
Lim Kha Shing <kslim5703@gmail.com>
2020-04-01 12:54:33 +0800

    Added ViewTreeObserver to wait seekbar view fully laid out

--- app/src/main/java/com/limkhashing/customseekbar/MainActivity.kt
+++ app/src/main/java/com/limkhashing/customseekbar/MainActivity.kt
@@ -7,11 +7,13 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
+import androidx.core.view.doOnLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.abs
 
@@ -33,24 +35,24 @@ class MainActivity : AppCompatActivity() {
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,10F)
 
                 seekbar.measure(0 , 0)
-//                Log.d("MainActivity", "Seekbar width = " + seekbar.width)
 
-                textView.measure(0, 0)
-//                Log.d("MainActivity", "Textview width = " + textView.measuredWidth.toString())
-//                Log.d("MainActivity", "String width = " + Paint().measureText(textView.text.toString()).toString())
+                seekbar.afterLayout {
+                    Log.d("MainActivity", "Seekbar width = " + seekbar.width)
 
-                val paddingStart = (1440 * (3/10F)) - (textView.measuredWidth / 2F)
-                Log.d("MainActivity", (3/10F).toString())
-                Log.d("MainActivity", (1440 * (3/10F)).toString())
-                Log.d("MainActivity", (textView.measuredWidth / 2F).toString())
-//                Log.d("MainActivity", "Total padding start = " + paddingStart.toString())
+                    textView.measure(0, 0)
+                    Log.d("MainActivity", "Textview width = " + textView.measuredWidth.toString())
+                    Log.d("MainActivity", "String width = " + Paint().measureText(textView.text.toString()).toString())
 
-                val set = ConstraintSet()
-                layout.addView(textView)
-                set.clone(layout)
-                set.connect(textView.id, ConstraintSet.TOP, seekbar.id, ConstraintSet.BOTTOM, 16)
-                set.connect(textView.id, ConstraintSet.START, seekbar.id, ConstraintSet.START, paddingStart.toInt())
-                set.applyTo(layout)
+                    val paddingStart = (seekbar.width * (3/10F)) - (textView.measuredWidth / 2F)
+                    Log.d("MainActivity", "Total padding start = " + paddingStart.toString())
+
+                    val set = ConstraintSet()
+                    layout.addView(textView)
+                    set.clone(layout)
+                    set.connect(textView.id, ConstraintSet.TOP, seekbar.id, ConstraintSet.BOTTOM, 16)
+                    set.connect(textView.id, ConstraintSet.START, seekbar.id, ConstraintSet.START, paddingStart.toInt())
+                    set.applyTo(layout)
+                }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -62,5 +64,18 @@ class MainActivity : AppCompatActivity() {
     fun dpToPixels(display_metrics: DisplayMetrics, dps: Float): Int {
         val scale = display_metrics.density
         return (dps * scale + 0.5f).toInt()
+    }
+}
+
+fun View.afterLayout(what: () -> Unit) {
+    if(isLaidOut) {
+        what.invoke()
+    } else {
+        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+                what.invoke()
+            }
+        })
     }
 }
